6aa29958a821c4a61d630e7733b6b7887b89b849
Vitor Cordeiro <vitor.cordeiro@b2wdigital.com>
2020-01-23 18:01:33 -0300

    Ajustes gerais

--- lambdas/readFromDynamoSendToRdsLambda.go
+++ lambdas/readFromDynamoSendToRdsLambda.go
@@ -15,6 +15,30 @@ package main
 
 	Obs.: Remember to give SecretsManagerReadWrite, AWSLambdaDynamoDBExecutionRole and
 	AmazonRDSDataFullAccess policies to your Lambda Function
+
+ 	=======================================================
+    Aurora RDS Script:
+	=======================================================
+	
+	drop table nickname;
+	drop table client;
+
+	create table client (
+		id varchar(100) NOT NULL PRIMARY KEY,
+		name varchar(100) not null,
+		email varchar(100) not null
+	);
+
+	create table nickname (
+		id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		name varchar(100) not null,
+		data date not null,
+		client_id varchar(100) NOT NULL,
+		CONSTRAINT FK_Client1 FOREIGN KEY (client_id) REFERENCES client(id)
+	);
+
+	insert into client(id, name,email) values ('234329489012839021','teste','teste');
+	insert into nickname(name,data,client_id) values ('teste','2008-01-01 00:00:01','234329489012839021');
 */
 
 import (
@@ -29,46 +53,55 @@ import (
 )
 
 type Client struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Items    *[]Nickname `json:"items"`
+}
+
+type Nickname struct {
+	Data string `json:"data"`
 	Name string `json:"name"`
-	Email string `json:"email"`
 }
 
 func main() {
 	lambda.Start(handleRequest)
 }
 
+func sendToAurora(SQLStatement string){
+	sess, _ := session.NewSession(&aws.Config{Region: aws.String("us-east-1")},)
+	rdsdataservice_client := rdsdataservice.New(sess)
+
+	fmt.Println("statement:", SQLStatement)
+
+	// The SecretArn is generate when you create a secret for db admin user at 'AWS Secrets Manager' 
+	req, resp := rdsdataservice_client.ExecuteStatementRequest(&rdsdataservice.ExecuteStatementInput{
+		Database:    aws.String("TABLE_NAME"),
+		ResourceArn: aws.String("DATABASE_ARN"),
+		SecretArn:   aws.String("ADMIN_USER_SECRET_ARN"),
+		Sql:         aws.String(SQLStatement),
+	})
+
+	err1 := req.Send()
+	if err1 == nil { 
+		fmt.Println("Response:", resp)
+	} else {
+		fmt.Println("error:", err1)
+	}
+}
+
 func handleRequest(ctx context.Context, e events.DynamoDBEvent) {
 
 	for _, record := range e.Records {
 		if record.EventName == "INSERT" {
+			SQLStatement := "insert into client(id,name,email) values ('" + record.Change.NewImage["id"].String() + "','" + record.Change.NewImage["name"].String() + "','" + record.Change.NewImage["email"].String() + "');"
 			
-			var client *Client
-			client = new(Client)
-			client.Name = record.Change.NewImage["name"].String()
-			client.Email = record.Change.NewImage["email"].String()
+			sendToAurora(SQLStatement)
 
-			sess, _ := session.NewSession(&aws.Config{
-				Region: aws.String("us-east-1")},
-			)
-		
-			rdsdataservice_client := rdsdataservice.New(sess)
-
-			SQLStatement := "insert into client(name,email) values ('" + client.Name + "','" + client.Email + "');"
-			fmt.Println("statement:", SQLStatement)
-			
-			// The SecretArn is generate when you create a secret for db admin user at 'AWS Secrets Manager' 
-			req, resp := rdsdataservice_client.ExecuteStatementRequest(&rdsdataservice.ExecuteStatementInput{
-				Database:    aws.String("TABLE_NAME"),
-				ResourceArn: aws.String("DATABASE_ARN"),
-				SecretArn:   aws.String("ADMIN_USER_SECRET_ARN"),
-				Sql:         aws.String(SQLStatement),
-			})
-		
-			err1 := req.Send()
-			if err1 == nil { 
-				fmt.Println("Response:", resp)
-			} else {
-				fmt.Println("error:", err1)
+			for _, value := range record.Change.NewImage["items"].List() {
+				nickname := value.Map()
+				SQLStatement = "insert into nickname(name,data,client_id) values ('" + nickname["name"].String() + "','" +nickname["data"].String() + "','" + record.Change.NewImage["id"].String() + "');"
+				sendToAurora(SQLStatement)
 			}
 
 		}else{
